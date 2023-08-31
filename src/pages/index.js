129b1545b2e3cf399c9dbb85d6ce47cf77d9e9ea
Satayjit-Biswas <satayjitbiswas2@gmail.com>
2023-08-31 17:38:31 +0600

    Add Git auth

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,20 @@
+import { useSession } from "next-auth/react";
 import Head from "next/head";
 const HomePage = () => {
-  return (
-    <div>
-      <Head>
-        <title>Next Auth</title>
-      </Head>
-      <h1 style={{ textAlign: "center", marginTop:"10%" }}>Welcome To Next Auth Home Page</h1>
-    </div>
-  );
+    const { data: session } = useSession();
+    return (
+        <div>
+            <Head>
+                <title>Next Auth</title>
+            </Head>
+            <h1 style={{ textAlign: "center", marginTop: "10%" }}>
+                Welcome To Next Auth Home Page
+            </h1>
+            <h2 style={{ textAlign: "center" }}>
+                User Name :{session?.user?.name}
+            </h2>
+        </div>
+    );
 };
 
 export default HomePage;
